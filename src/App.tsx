2d90b6aefabae7e08c97c2c8a69dc06eff00adea
pedroonovais <phenrique101007@gmail.com>
2024-08-27 13:08:48 -0300

    Cria estrutura de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const LazyHome = lazy(() => import("./pages/Home"));
+
 function App() {
-  return (
-    <>
-      <p> Web Site Improve it usando react-ts</p>
-    </>
-  )
+    return(
+        <BrowserRouter>
+            <Suspense fallback={<>Carregando</>}>
+                <Routes>
+                    <Route path="/" element={<LazyHome />} />
+                </Routes>
+            </Suspense>
+        </BrowserRouter>
+    )
 }
 
-export default App
+export default App;
